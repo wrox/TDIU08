121e0e8f2aa494a78f67d43c88c4df7549b38dd2
Wrox <wrox@users.noreply.github.com>
2020-01-29 15:57:32 +0100

    Create calculator.adb

---
+++ LAB2_B/calculator.adb
@@ -0,0 +1,103 @@
+with Ada.Text_IO;			use Ada.Text_IO;
+with Ada.Float_Text_IO;			use Ada.Float_Text_IO;
+with Ada.Integer_Text_IO;		use Ada.Integer_Text_IO;
+with fFactorial;
+
+procedure calculator is   
+     
+   -- Operatorer
+   function "+"(fX: in Float; iA: in Integer) return Float is
+      fY: Float;
+   begin
+      fY := Float(iA);
+      return fX + fY;
+   end "+";   
+   
+   function "+"(iA: in Integer; fX: in Float) return Float is
+      fY: Float;
+   begin
+      -- return fX + iA;
+      fY := Float(iA);
+      return fX + fY; 
+   end "+";
+   
+   Choice : Integer;
+   A, B   : Integer;
+   X, Y   : Float;
+begin
+   Put_Line("Välkommen till miniräknaren!");
+   loop
+      Put_Line("1. Beräkna N-fakultet.");
+      Put_Line("2. Addera två heltal.");
+      Put_Line("3. Addera heltal med flyttal.");
+      Put_Line("4. Addera flyttal med heltal.");
+      Put_Line("5. Addera två flyttal.");
+      Put_Line("6. Avsluta.");
+      Put("Välj ett alternativ: ");
+      Get(Choice);
+      New_Line;
+      exit when Choice = 6;
+      
+      if Choice = 1 then
+	 Put("Mata in ett heltal: ");
+	 Get(A);
+
+	 Put(A, Width => 0);
+	 Put("! = ");
+	 ---
+	 Put(fFactorial(A), Width=>0);
+	 
+      elsif Choice = 2 then
+	 Put("Mata in två heltal: ");
+	 Get(A);
+	 Get(B);
+	 
+	 Put(A, Width => 0);
+	 Put(" + ");
+	 Put(B, Width => 0);
+	 Put(" = ");
+	 ---
+	 Put(A + B, Width=>0);
+	 
+      elsif Choice = 3 then
+	 Put("Mata in ett heltal och ett flyttal: ");
+	 Get(A);
+	 Get(X);
+	 
+	 Put(A, Width => 0);
+	 Put(" + ");
+	 Put(X, Fore => 0, Aft => 2, Exp => 0);
+	 Put(" = ");
+	 ---
+	 Put(A + X, Fore=>0, Aft=>2, Exp=>0);
+	 
+      elsif Choice = 4 then
+	 Put("Mata in ett flyttal och ett heltal: ");
+	 Get(X);
+	 Get(A);
+	 
+	 Put(X, Fore => 0, Aft => 2, Exp => 0);
+	 Put(" + ");
+	 Put(A, Width => 0);
+	 Put(" = ");
+	 ---
+	 Put(X + A, Fore=>0, Aft=>2, Exp=>0);
+	 
+      elsif Choice = 5 then
+	 Put("Mata in två flytal: ");
+	 Get(X);
+	 Get(Y);
+	 
+	 Put(X, Fore => 0, Aft => 2, Exp => 0);
+	 Put(" + ");
+	 Put(Y, Fore => 0, Aft => 2, Exp => 0);
+	 Put(" = ");
+	 ---
+	 Put(X + Y, Fore=>0, Aft=>2, Exp=>0);	 
+	 
+      end if;
+      New_Line(2);
+   end loop;
+   Put_Line("Hejdå, vi ses nästa gång!");
+      
+end calculator;
