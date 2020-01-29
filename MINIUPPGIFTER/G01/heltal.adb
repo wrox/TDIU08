14ac5371ce472cc621c0fe9352b088585ac2f26f
Wrox <wrox@users.noreply.github.com>
2020-01-29 16:03:25 +0100

    Create heltal.adb

---
+++ MINIUPPGIFTER/G01/heltal.adb
@@ -0,0 +1,31 @@
+with Ada.Text_IO;          use Ada.Text_IO;
+with Ada.Integer_Text_IO;  use Ada.Integer_Text_IO;
+
+procedure Heltal is
+   
+   iCounter : Integer := 1;
+   Space : Character := ' ';
+   
+   begin
+      Put_Line("For loop: ");
+      for I in 1..10 loop
+	 Put(I, Width=>0);
+	 Put(Space);
+      end loop;
+      New_Line;
+      Put_Line("While loop:");
+      while iCounter <= 10 loop
+	 Put(iCounter, Width=>0);
+	 iCounter := iCounter + 1;
+	 Put(Space);
+      end loop;
+      New_Line;
+      iCounter := 1;
+      Put_Line("Loop:");
+      loop
+	 Put(iCounter, Width=>0);
+	 iCounter := iCounter +1;
+	 Put(Space);
+	 exit when iCounter = 11;
+      end loop;
+   end Heltal;
